fd56eecb026c767b951e7a3e1ce049b9d42e4c20
Lucasmck14 <lucasmck14@gmail.com>
2025-06-09 13:30:53 -0400

    feat(frontend): created simple form in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={e => setFile(e.target.files[0])} />
+      <button type="submit">Upload</button>
+    </form>
   );
 }
 
