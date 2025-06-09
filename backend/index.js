0025988e0ee74a232548de1b09d5539680a88d5e
Lucasmck14 <lucasmck14@gmail.com>
2025-06-09 13:26:10 -0400

    feat(backend): built basic backend structure

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const uploadRoutes = require('./routes/uploadRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api/upload', uploadRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
