fa386e878afc5273beafefeb0aca8c45dad42810
Lucasmck14 <lucasmck14@gmail.com>
2025-06-11 12:21:56 -0400

    chore(frontend): changed file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import UploadForm from './UploadForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UploadPage from './pages/UploadPage';
+import FilesPage from './pages/FilesPage';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <UploadForm />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/files" element={<FilesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/UploadPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UploadPage() {
+  return (
+    <div>
+      <h2>Upload a file</h2>
+    </div>
+  );
+}
