b915949e2a950a867ebf68a6d921d8f2f3713e5a
Lucasmck14 <lucasmck14@gmail.com>
2025-06-15 13:47:16 -0600

    chore(backend): added documentation to formRoutes.js

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -1,7 +1,14 @@
+// files general purpose: define route for manual data entry
+
+// import the Express library to define routes
 const express = require('express');
+// create new router instance - this allows organization of routes in seperate files instead of one file
 const router = express.Router();
+// import function for handling form submission
 const { handleFormSubmission } = require('../controllers/formController');
 
+// forward data entered to handleFormSubmission for handling
 router.post('/form', handleFormSubmission);
 
+// export router object for use in other files
 module.exports = router;
