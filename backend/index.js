aa26cfdeda511235a5f29d90101c1491cedf5784
Lucasmck14 <lucasmck14@gmail.com>
2025-06-17 10:23:46 -0600

    feat(backend): fixed issue of users not being able to download files

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,17 @@ const PORT = process.env.PORT || 5001;
 
 // enable cors for all routes and methods
 app.use(cors());
-// middleware that parses incoming requests with JSON payloads.
+// middleware that parses incoming requests with JSON payloads
 app.use(express.json());
+// allow users to download files stored in the 'uploads' folder
+app.use('/uploads', express.static('uploads'));
 
 // mount routes at root path
 app.use('/', uploadRoutes);
 app.use('/', filesRoutes);
 app.use('/', formRoutes);
+
+
 
 // log used to tell which port server is running on
 app.listen(PORT, () => {
